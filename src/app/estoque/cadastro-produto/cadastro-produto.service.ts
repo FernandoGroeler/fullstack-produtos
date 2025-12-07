e91faf8bd2b4c984da00840b7be229d4f5657dd5
Fernando Groeler <fernando.groeler@gmail.com>
2025-12-07 18:25:59 -0400

    Incluído environments

--- src/app/estoque/cadastro-produto/cadastro-produto.service.ts
+++ src/app/estoque/cadastro-produto/cadastro-produto.service.ts
@@ -3,27 +3,28 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CadastroProduto } from './cadastro-produto';
 import { ApiResponse } from '../../common/api-response.model';
-import { Router } from '@angular/router';
-
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 
 export class CadastroProdutoService {
+  private baseUrl = environment.apiUrl;
+
   constructor(
     private http: HttpClient
   ) { }
 
   salvar(cadastroProduto: CadastroProduto) : Observable<ApiResponse<CadastroProduto>> {
     if (cadastroProduto.id) {
-      return this.http.put<ApiResponse<CadastroProduto>>(`http://localhost:5021/api/v1/produto`, cadastroProduto);
+      return this.http.put<ApiResponse<CadastroProduto>>(`${this.baseUrl}/api/v1/produto`, cadastroProduto);
     }
 
-    return this.http.post<ApiResponse<CadastroProduto>>('http://localhost:5021/api/v1/produto', cadastroProduto);
+    return this.http.post<ApiResponse<CadastroProduto>>(`${this.baseUrl}/api/v1/produto`, cadastroProduto);
   }
 
   obterProdutoPorId(id: string) : Observable<ApiResponse<CadastroProduto>> {
     let params = new HttpParams().set('id', id);
-    return this.http.get<ApiResponse<CadastroProduto>>('http://localhost:5021/api/v1/produto', { params: params });
+    return this.http.get<ApiResponse<CadastroProduto>>(`${this.baseUrl}/api/v1/produto`, { params: params });
   }
 }
--- src/app/estoque/consulta-produto/consulta-produto.service.ts
+++ src/app/estoque/consulta-produto/consulta-produto.service.ts
@@ -3,20 +3,23 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ConsultaProduto } from './consulta-produto';
 import { ApiResponse } from '../../common/api-response.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class ConsultaProdutoService {
+  private baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   listarTodos(): Observable<ApiResponse<ConsultaProduto[]>> {
-    return this.http.get<ApiResponse<ConsultaProduto[]>>('http://localhost:5021/api/v1/listar-todos-produtos');
+    return this.http.get<ApiResponse<ConsultaProduto[]>>(`${this.baseUrl}/api/v1/listar-todos-produtos`);
   }
 
   excluirProduto(id: string) : Observable<ApiResponse<boolean>> {
     let params = new HttpParams().set('id', id);
-    return this.http.delete<ApiResponse<boolean>>('http://localhost:5021/api/v1/produto', { params: params });
+    return this.http.delete<ApiResponse<boolean>>(`${this.baseUrl}/api/v1/produto`, { params: params });
   }
 }
